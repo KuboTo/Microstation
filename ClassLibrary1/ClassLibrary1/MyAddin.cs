c64d03888a73e7e3ccf9282a7891248e361319d8
KuboTo <wangdengcheng1993@163.com>
2017-05-12 18:44:27 +0800

    创建元素
    包括两部分，如何开始MyAddin 以及创建元素CreateElement

---
+++ ClassLibrary1/ClassLibrary1/MyAddin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Bentley.Internal.MicroStation.Elements;
+
+
+namespace ClassLibrary1
+{
+    internal sealed class MyAddin:Bentley.MicroStation.AddIn
+    {
+        private MyAddin(System.IntPtr mdlDesc) : base(mdlDesc)
+        {
+            
+        }
+        protected override int Run(string[] commandLine)
+        {
+            //string sWinFrameworkPath = RuntimeEnvironment.GetRuntimeDirectory();
+            //MessageBox.Show("Framework Path =" + sWinFrameworkPath);
+            CreateElement.LineAndLineString();
+            CreateElement.ShapeAndComplexShape();
+            CreateElement.TextAndTextNode();
+            CreateElement.CellAndSharedCell();
+            CreateElement.LinearAndAngularDimension();
+            CreateElement.CurveAndBsplineCurve();
+            CreateElement.ConeAndBsplineSurface();
+            return 0;
+
+        }
+    }
+}
